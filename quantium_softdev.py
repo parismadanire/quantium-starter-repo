33d4dd15ef3915b27067e451f2f1530574ad2def
parismadanire <parismadanire@gmail.com>
2025-03-02 14:17:42 -0500

    step2

---
+++ quantium_softdev.py
@@ -0,0 +1,26 @@
+import csv
+import pandas as pd
+import dash as ds
+
+df =pd.read_csv('daily_sales_data_0.csv', header= 0)
+df1 =pd.read_csv('daily_sales_data_1.csv', header =0)
+df2 =pd.read_csv('daily_sales_data_2.csv', header =0)
+
+
+def sales(df, df1, df2):
+    for dataframe in [df, df1, df2]:
+        dataframe['price']= dataframe['price'].replace({r"\$": '', r"," :""}, regex = True).astype(float)
+        dataframe['sales'] = dataframe['price'] * dataframe['quantity']
+    combined_df =pd.concat([df, df1, df2], ignore_index = True)
+    
+    pink_morsel_df =combined_df[combined_df['product'].str.strip().str.lower() == "pink morsel"]
+    return pink_morsel_df[['sales', 'date', 'region']]
+
+print(sales(df, df1, df2))
+
+
+
+
+
+
+
